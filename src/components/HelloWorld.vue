2de184b9eaf13f16770218e9d464627dd8facbde
Ku bonsang <165915671+Kubonsang@users.noreply.github.com>
2024-05-14 18:29:16 +0900

    기본 세팅
    .

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <NameCard></NameCard>
+    <AboutMe></AboutMe>
+    <Info></Info>
+    <Contact></Contact>
+    <Education></Education>
+  </div>
+</template>
+
+<script>
+// 포트폴리오 제목 컴포넌트
+import NameCard from './NameCard.vue'
+// 자기 소개 컴포넌트
+import AboutMe from './AboutMe.vue'
+// 정보 컴포넌트
+import Info from './Info.vue'
+// 연락처 컴포넌트
+import Contact from './Contact.vue'
+// 교육 컴포넌트
+import Education from "./Education.vue";
+
+export default {
+  name: 'App',
+  components: {
+    NameCard,
+    Info,
+    AboutMe,
+    Contact,
+    Education,
+  }
+}
+</script>
---
+++ src/components/NameCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>홍길동의 Portfolio</h1>
+    </div>
+  </template>
+  
+  <script>
+  //Vue.js 컴포넌트를 export합니다. 
+  export default {
+    name: 'NameCard'
+  }
+  </script>
+  
+  <style scoped>
+  div {
+    text-align: left;
+    border-bottom: 1px solid gray;
+  }
+  
+  </style>
+  
